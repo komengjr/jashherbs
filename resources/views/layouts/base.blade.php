6d4051ebd5deaf89bf18d224d12cf10ab61f536d
komengjr <47206605+komengjr@users.noreply.github.com>
2025-11-11 20:17:13 +0700

    uploadall

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,6 +22,9 @@
     <link rel="stylesheet" href="{{ url('css/owl.carousel.min.css', []) }}" type="text/css">
     <link rel="stylesheet" href="{{ url('css/slicknav.min.css', []) }}" type="text/css">
     <link rel="stylesheet" href="{{ url('css/style.css', []) }}" type="text/css">
+    <meta name="google-adsense-account" content="ca-pub-4154628728879232">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4154628728879232"
+     crossorigin="anonymous"></script>
 </head>
 
 <body>
